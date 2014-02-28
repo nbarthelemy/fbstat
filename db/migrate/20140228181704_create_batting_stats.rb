20bef776a0ce60378d12adb4c0a4305fa072ea1f
Nicholas Barthelemy <nicholas.barthelemy@gmail.com>
2014-02-28 13:17:04 -0600

    Create batting stats table

---
+++ db/migrate/20140228181704_create_batting_stats.rb
@@ -0,0 +1,19 @@
+class CreateBattingStats < ActiveRecord::Migration
+  def change
+    create_table :batting_stats do |t|
+      t.references :player
+      t.references :team
+      t.integer :year
+      t.integer :games_played
+      t.integer :at_bats
+      t.integer :runs
+      t.integer :hits
+      t.integer :doubles
+      t.integer :triples
+      t.integer :homeruns
+      t.integer :rbis
+      t.integer :stolen_bases
+      t.integer :caught_stealing
+    end
+  end
+end

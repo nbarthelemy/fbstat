5098cabb9d7d50dbddd049d91898257bc60cd4a0
Nicholas Barthelemy <nicholas.barthelemy@gmail.com>
2014-02-28 13:16:52 -0600

    Create teams table

---
+++ db/migrate/20140228181521_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration
+
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :league
+      t.string :code
+    end
+  end
+
+end

6571e0715060739668aa4d7fe678d1efb976fefb
Nicholas Barthelemy <nicholas.barthelemy@gmail.com>
2014-03-03 12:20:10 -0600

    Add batting stats calculations

--- app/models/batting_stat.rb
+++ app/models/batting_stat.rb
@@ -5,10 +5,89 @@ class BattingStat < ActiveRecord::Base
   belongs_to :player
   belongs_to :team
 
-
   ## Validations
 
   validates :player_id, :team_id, :year, presence: true
   validates :player_id, uniqueness: { scope: [ :team_id, :year ] }
+
+  ## Scopes
+
+  scope :for_league, lambda{|league, year|
+    includes(:team, :player).where('teams.league = ? AND batting_stats.year = ?', league, year).
+      order('players.last_name, players.first_name').references(:teams)
+  }
+
+  scope :for_team, lambda{|team_code, year|
+    includes(:team, :player).where('teams.code = ? AND batting_stats.year = ?', team_code, year).
+      order('players.last_name, players.first_name').references(:teams)
+  }
+
+  scope :min_at_bats, lambda{|min_at_bats, start_year, end_year = nil|
+    where('at_bats >= ? AND year IN (?)', min_at_bats, [ start_year, end_year ].compact).
+      order('player_id, year desc')
+  }
+
+  ## Class Methods
+
+  class << self
+
+    def most_improved_batting_average(start_year, end_year, min_at_bats = 200)
+      most_improved :batting_average, start_year, end_year, min_at_bats
+    end
+
+    def most_improved_fantasy_points(start_year, end_year, min_at_bats = 500)
+      most_improved :fantasy_points, start_year, end_year, min_at_bats
+    end
+
+    def triple_crown_winner(league, year)
+      # full major season consists of 162 games, 162 multiplied by 3.1 equals 502
+      # 502 is the required minimum at_bats to with the triple crown
+      season_games = 162
+      at_bats_per_game = 3.1
+
+      stats = for_league(league, year).where('at_bats >= ?', season_games * at_bats_per_game).load
+
+      candidates = []
+      candidates << stats.sort_by(&:batting_average).last
+      candidates << stats.sort_by(&:homeruns).last
+      candidates << stats.sort_by(&:rbis).last
+      candidates.compact!
+
+      if candidates.length > 0 && candidates.group_by(&:player_id).first.last.count == 3
+        candidates.first.player
+      end
+    end
+
+  private
+
+    def most_improved(stat, start_year, end_year, min_at_bats)
+      records = min_at_bats(min_at_bats, start_year, end_year).group_by(&:player_id).select{|r, s| s.count > 1 }
+      records.collect{|pid, stats|
+        { player_id: pid, value: ( stats.first.send(stat) * 1.0 / stats.last.send(stat) ).to_f }
+      }.sort_by{|r| r[:value] }.reverse
+    end
+
+  end
+
+  ## Instance Methods
+
+  def fantasy_points
+    [ ( homeruns.to_i * 4 ), rbis.to_i, stolen_bases.to_i ].sum - caught_stealing.to_i
+  end
+
+  def batting_average
+    n = at_bats.to_f
+    n > 0 ? ( hits.to_f / n ) : 0
+  end
+
+  def slugging_percentage
+    n = at_bats.to_f
+    n > 0 ? ([ 
+      ( hits.to_i - doubles.to_i - triples.to_i - homeruns.to_i ), 
+      ( 2 * doubles.to_i ), 
+      ( 3 * triples.to_i ), 
+      ( 4 * homeruns.to_i )
+    ].sum / n ).round(3) : 0
+  end
 
 end
--- spec/models/batting_stat_spec.rb
+++ spec/models/batting_stat_spec.rb
@@ -15,4 +15,85 @@ describe BattingStat do
     it { should validate_uniqueness_of(:player_id).scoped_to([ :team_id, :year ]) }
   end
 
+  context :most_improved do
+    before(:each) do
+      @p1 = Fabricate(:player, code: 'P1')
+      @p1.batting_stats << Fabricate(:batting_stat, year: 2009, at_bats: 510, hits: 110, rbis: 35)
+      @p1.batting_stats << Fabricate(:batting_stat, year: 2010, at_bats: 515, hits: 200, rbis: 45)
+      @p1.batting_stats << Fabricate(:batting_stat, year: 2011, at_bats: 505, hits: 125, rbis: 50)
+      @p1.batting_stats << Fabricate(:batting_stat, year: 2012, at_bats: 555, hits: 155, rbis: 62)
+
+      @p2 = Fabricate(:player, code: 'P2')
+      @p2.batting_stats << Fabricate(:batting_stat, year: 2009, at_bats: 510, hits: 110, rbis: 20)
+      @p2.batting_stats << Fabricate(:batting_stat, year: 2010, at_bats: 560, hits: 200, rbis: 30)
+      @p2.batting_stats << Fabricate(:batting_stat, year: 2011, at_bats: 500, hits: 130, rbis: 35)
+      @p2.batting_stats << Fabricate(:batting_stat, year: 2012, at_bats: 550, hits: 140, rbis: 45)
+
+      @p3 = Fabricate(:player, code: 'P3')
+      @p3.batting_stats << Fabricate(:batting_stat, year: 2009, at_bats: 500, hits: 100, rbis: 35)
+      @p3.batting_stats << Fabricate(:batting_stat, year: 2010, at_bats: 520, hits: 200, rbis: 55)
+      @p3.batting_stats << Fabricate(:batting_stat, year: 2011, at_bats: 510, hits: 145, rbis: 45)
+      @p3.batting_stats << Fabricate(:batting_stat, year: 2012, at_bats: 560, hits: 150, rbis: 55)
+    end
+
+    it "should have the most improved batting average between 2009 and 2010" do
+      BattingStat.most_improved_batting_average(2009, 2010).first[:player_id].should == @p3.id
+    end
+
+    it "should have the most improved fantasy points between 2011 and 2012" do
+      BattingStat.most_improved_fantasy_points(2011, 2012).first[:player_id].should == @p2.id
+    end
+  end
+
+  context :triple_crown_winner do
+    before(:each) do
+      @p1 = Fabricate(:player, code: 'P1')
+      @p1.batting_stats << Fabricate(:batting_stat, year: 2012, at_bats: 550, hits: 130, homeruns: 15, rbis: 25)
+
+      @p2 = Fabricate(:player, code: 'P2')
+      @p2.batting_stats << Fabricate(:batting_stat, year: 2012, at_bats: 550, hits: 155, homeruns: 10, rbis: 30)
+
+      @p3 = Fabricate(:player, code: 'P3')
+      @p3.batting_stats << Fabricate(:batting_stat, year: 2012, at_bats: 550, hits: 160, homeruns: 25, rbis: 55)
+    end
+
+    it "should find the triple crown winner" do
+      BattingStat.triple_crown_winner('American', 2012).should == @p3
+    end
+  end
+
+  context :fantasy_points do
+    before(:each) do
+      @player = Fabricate(:player, code: 'P1')
+    end
+
+    it "should have fantasy points" do
+      @batting_stat = Fabricate(:batting_stat, year: 2012, homeruns: 25, rbis: 100, 
+        stolen_bases: 5, caught_stealing: 5)
+      @batting_stat.fantasy_points.should == 200
+    end
+  end
+
+  context :batting_average do
+    before(:each) do
+      @player = Fabricate(:player, code: 'P1')
+    end
+
+    it "should have a batting average" do
+      Fabricate(:batting_stat, year: 2012, at_bats: 500, hits: 100).batting_average.should == 0.200
+    end
+  end
+
+  context :slugging_percentage do
+    before(:each) do
+      @player = Fabricate(:player, code: 'P1')
+    end
+
+    it "should have a slugging percentage" do
+      @batting_stat = Fabricate(:batting_stat, year: 2012, at_bats: 500, 
+        hits: 180, doubles: 25, triples: 5, homeruns: 15)
+      @batting_stat.slugging_percentage.should == 0.52
+    end
+  end
+
 end
